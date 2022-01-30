b944df028e9985e60b5b9cadaeb2e59c02d07ee4
Beatrice <beatricemmunteanu@gmail.com>
2022-01-30 16:01:46 +0200

    add more login fixes

--- src/main/java/com/example/eatsy/controller/AuthController.java
+++ src/main/java/com/example/eatsy/controller/AuthController.java
@@ -27,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -51,32 +52,25 @@ public class AuthController {
     public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
 
         Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+                new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
         String jwt = jwtUtils.generateJwtToken(authentication);
 
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-        List<String> roles = userDetails.getAuthorities().stream()
-                .map(item -> item.getAuthority())
-                .collect(Collectors.toList());
+        String role = String.valueOf(userDetails.getAuthorities().iterator().next());
 
         return ResponseEntity.ok(new JwtResponse(jwt,
                 userDetails.getId(),
-                userDetails.getUsername(),
+                userDetails.getFirstName(),
+                userDetails.getLastName(),
                 userDetails.getEmail(),
-                roles));
+                role));
     }
 
     @PostMapping("/signup")
     @Operation(description = "Role: admin/restaurant manager/delivery man/customer")
     public ResponseEntity<?> registerUser( @RequestBody SignupRequest signUpRequest) {
-        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
-            return ResponseEntity
-                    .badRequest()
-                    .body("Error: Username is already taken!");
-        }
-
         if (userRepository.existsByEmail(signUpRequest.getEmail())) {
             return ResponseEntity
                     .badRequest()
--- src/main/java/com/example/eatsy/dto/JwtResponse.java
+++ src/main/java/com/example/eatsy/dto/JwtResponse.java
@@ -1,6 +1,7 @@
 package com.example.eatsy.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,10 +10,12 @@ import java.util.List;
 @Getter
 @Setter
 @AllArgsConstructor
+@Builder
 public class JwtResponse {
     private String token;
     private Long id;
-    private String username;
+    private String firstName;
+    private String lastName;
     private String email;
-    private List<String> roles;
+    private String role;
 }
